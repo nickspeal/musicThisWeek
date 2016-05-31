8d32a536274ab4292c93ae2c4678fc1c2ecf8228
Nick <Nick@Nicks-MacBook-Pro-3.local>
2016-05-30 21:24:15 -0700

    rm unused old files

--- old-news/scrap/scrape.py
+++
@@ -1,8 +0,0 @@
-'''Scrape webpage'''
-
-from bs4 import BeautifulSoup
-
-from subprocess import check_output
-
-soup = BeautifulSoup(check_output(['curl', "http://www.sfweekly.com/concerts/"]))
-print soup.prettify()
