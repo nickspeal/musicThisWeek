f4e2e7a13e986cbdb63333ba5802b18086ef6dd9
Nick Speal <nick@speal.ca>
2016-07-09 15:17:54 -0700

    update allowed_hosts to include server

--- musicThisWeek/settings.py
+++ musicThisWeek/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['159.203.246.166']
 
 
 # Application definition
