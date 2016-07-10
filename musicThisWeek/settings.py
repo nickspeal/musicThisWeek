2791afd3dca47bed9ec5c36cf7bf097abe71dd25
Nick Speal <nick@speal.ca>
2016-07-09 17:02:35 -0700

    enable debug mode for local testing

--- musicThisWeek/settings.py
+++ musicThisWeek/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['159.203.246.166', 'mtw.speal.ca']
+ALLOWED_HOSTS = ['159.203.246.166', 'mtw.speal.ca', 'localhost']
 
 
 # Application definition
