77e59e6e5c89c2624f143cddce82c349a3fd1b51
Edward Mason <esmason66@gmail.com>
2016-09-11 22:13:59 -0700

    stop someone from accidentally deleting the project

--- downloadGeo.sh
+++ downloadGeo.sh
@@ -2,5 +2,6 @@ curl -O geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
 curl -O geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz 
 gunzip -c GeoLite2-City.mmdb.gz > _private/GeoLite2-City.mmdb
 gunzip -c GeoLite2-Country.mmdb.gz > _private/GeoLite2-Country.mmdb
-rm GeoLite2*
+rm GeoLite2-City.mmdb.gz
+rm GeoLite2-Country.mmdb.gz
 
