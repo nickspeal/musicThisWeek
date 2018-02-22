5ce52c29b985c0dc8e8fcaa04711e9b65fc0574d
Nick Speal <nick@speal.ca>
2018-02-21 22:31:06 -0800

    print out secret key as a test

--- musicThisWeek/wsgi.py
+++ musicThisWeek/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "musicThisWeek.settings")
 
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+print("Found this secret key as an environment variable: ", SECRET_KEY)
 application = get_wsgi_application()
